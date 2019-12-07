327b3da6050c9934625c93491c1181598ff6fd32
vorujack <vorujack@gmail.com>
2019-12-07 18:50:13 +0330

    add migration update production.py change cmd to Entrypoint in docker file fix models add blank and null to msg and tx_id

--- Api/models.py
+++ Api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Block(models.Model):
     public_key = models.TextField(unique=True)
-    msg = models.TextField()
-    tx_id = models.TextField()
+    msg = models.TextField(blank=True, null=True)
+    tx_id = models.TextField(blank=True, null=True)
     
     def __str__(self):
         return self.public_key
