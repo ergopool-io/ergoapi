c33b4b1cd8b024717c79916c7d0f33b5bc8c0895
vorujack <vorujack@gmail.com>
2019-12-07 18:50:13 +0330

    add migration update production.py change cmd to Entrypoint in docker file fix models add blank and null to msg and tx_id

--- config/production.py
+++ config/production.py
@@ -5,7 +5,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'admin',
+        'NAME': 'db_api',
         'USER': 'admin',
         'PASSWORD': 'admin',
         'HOST': 'db',  # same as the docker-compose service
@@ -17,3 +17,13 @@ ALLOWED_HOSTS = [os.environ.get('HOST')]
 
 # Access url for accounting system
 ACCOUNTING_URL = "http://" + os.environ.get("ACCOUNTING")
+
+
+# Difficulty of pool (pb) (ex: 799144731656113400000000000000000000000000000000000000000000000)
+POOL_DIFFICULTY = "7799144731656113400000000000000000000000000000000000000000000000"
+
+# Address Node (ex: "http://127.0.0.1:9053/")
+NODE_ADDRESS = os.environ.get("NODE")
+
+# Secret Key of Node(apiKey) (ex: "623f4e8e440007f45020afabbf56d8ba43144778757ea88497c794ad529a0433")
+API_KEY = os.environ.get("SECRET")
