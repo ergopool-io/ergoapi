3ed35eb7caeaf41b56274e42b92bcd1a8f7b3b25
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2020-02-03 04:39:00 +0000

    Resolve "Add more verification parameter to proof"

--- config/production.py
+++ config/production.py
@@ -27,3 +27,9 @@ NODE_ADDRESS = "http://%s:%s/" % (os.environ.get("NODE_HOST"), os.environ.get("N
 
 # Secret Key of Node(apiKey) (ex: "623f4e8e440007f45020afabbf56d8ba43144778757ea88497c794ad529a0433")
 API_KEY = os.environ.get("SECRET")
+
+# Explorer ergo (ex: https://api.ergoplatform.com/)
+ERGO_EXPLORER_ADDRESS = "https://%s/" % (os.environ.get("ERGO_EXPLORER_ADDRESS"))
+
+# Number of call logger in level critical
+NUMBER_OF_LOG = os.environ.get("NUMBER_OF_LOG")
