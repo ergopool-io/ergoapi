2aa907f99c8ef0b1a1f1e5d79af85591fbce10df
Mehran Aghabozorgi <mehranagh2000@gmail.com>
2020-02-27 12:17:38 +0000

    Resolve "ValidationSerializer must be tested!"

--- Api/models.py
+++ Api/models.py
@@ -1,15 +1,1 @@
-import logging
-
-from django.db import models
-
-logger = logging.getLogger(__name__)
-
-
-class Block(models.Model):
-    public_key = models.TextField(unique=True)
-    msg = models.TextField(blank=True, null=True)
-    tx_id = models.TextField(blank=True, null=True)
-    
-    def __str__(self):
-        return self.public_key
 
